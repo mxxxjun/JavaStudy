f39317a8b8b9511466ce7cc072093370f9c2f758
mxxxjun <mj991212@gachon.ac.kr>
2023-12-18 01:19:28 +0900

    스레드 마무리

---
+++ Thread/src/Synchronization/sync.java
@@ -0,0 +1,60 @@
+package Synchronization;
+
+public class sync {
+    public static void main(String[] args){
+        Memory mem = new Memory(16);
+        DownLoad down = new DownLoad(mem);
+        Play play = new Play(mem);
+
+        down.start();
+        play.start();
+    }
+}
+
+class Memory{
+    int[] buffer;
+    int size;
+    int progress;
+    Memory(int size){
+        this.size = size;
+        this.buffer = new int[size];
+        this.progress = 0;
+    }
+}
+
+class DownLoad extends Thread{
+    Memory mem;
+    DownLoad(Memory mem){
+        this.mem = mem;
+    }
+    public void run(){
+        for(int off=0; off < mem.size; off += 2){
+            for(int chunk = 0; chunk < 2; chunk++){
+                mem.buffer[off+chunk] = off+chunk;
+                mem.progress = off + chunk + 1;
+                try{
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {}
+            }
+        }
+    }
+}
+
+class Play extends Thread{
+    Memory mem;
+    Play(Memory mem){
+        this.mem = mem;
+    }
+    public void run(){
+        for(;;){
+            for(int off = 0; off < mem.progress; off++){
+                System.out.print(mem.buffer[off] + " ");
+            }
+            System.out.println();
+            if(mem.progress == mem.size) break;
+            try{
+                Thread.sleep(500);
+            } catch(InterruptedException e) {}
+        }
+    }
+}
